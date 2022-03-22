f4c88a03063c5886352dcaf5a99b6afe23faa436
sabbirpboss <sabbir.ah799@gmail.com>
2022-03-22 08:56:00 +0600

    46-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Hello World</h1>
+       <LoadCountries></LoadCountries>
     </div>
   );
+}
+
+const LoadCountries = () => {
+  const [countries, setCountries] = useState([]);
+  useEffect( () => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => setCountries(data))
+  },[])
+  return (
+    <div className="">
+      <h1>Visiting Every Country Of the World!!!</h1>
+      <h3>Available Countries: {countries.length}</h3>
+      {
+        countries.map(country => <Country name = {country.name.common} flags = {country.flags.png} capital = {country.capital} population = {country.population}></Country>)
+      }
+    </div>
+  )
+}
+
+const Country = (props) => {
+  return (
+    <div style={{backgroundColor: 'aqua', color: 'black', padding: '20px', margin: '20px', border: '3px solid tomato', borderRadius: '20px'}} className="">
+      <h2>Country Name: {props.name}</h2>
+      <img src="{props.flags}" alt="" />
+      <p>Capital: {props.capital}</p>
+      <p>Population: {props.population}</p>
+    </div>
+  )
 }
 
 export default App;
